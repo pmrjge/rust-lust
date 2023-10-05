014dc5eae98e78245e93c101e184be2d113890b6
Pedro Jorge <2230393@my.ipleiria.pt>
2023-10-05 09:20:36 +0100

    Initial examples using move, clone and ownership2

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let s = "hello";
+    let mut s = String::from("hello");
+    s.push_str(", world!");
+    println!("{}", s);
+
+    let x = 5;
+    let y = x;
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {s1}, s2 = {s2}");
+
+
+
 }
