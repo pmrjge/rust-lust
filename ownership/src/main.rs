44d53994f58fe7f249e0014dd24ec89ae1e135a6
Pedro Jorge <2230393@my.ipleiria.pt>
2023-10-05 09:30:50 +0100

    Moving and returning ownership at the same time

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,20 +1,51 @@
 fn main() {
-    let s = "hello";
+    let _s = "hello";
     let mut s = String::from("hello");
     s.push_str(", world!");
     println!("{}", s);
 
     let x = 5;
-    let y = x;
+    let _y = x;
 
     let s1 = String::from("hello");
-    let s2 = s1;
+    let _s2 = s1;
 
     let s1 = String::from("hello");
     let s2 = s1.clone();
 
     println!("s1 = {s1}, s2 = {s2}");
 
+    let x = 5;
+    let y = x;
+    println!("x = {x}, y={y}");
+
+    // Ownership and Functions
+    let s = String::from("hello");
+
+    takes_ownership(s);
+
+    let x = 5;
+    makes_copy(x);
+    let s1 = gives_ownership();
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
+
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
 
 
+fn takes_ownership(some_string: String) {
+    println!("{some_string}");
+}
+
+fn makes_copy(some_integer: i32){
+    println!("{some_integer}");
 }
