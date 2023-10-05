2f09728a9f1783881e97c75afb1e3c895ebffe6a
Pedro Jorge <2230393@my.ipleiria.pt>
2023-10-05 09:49:30 +0100

    References and borrowing, mutable references

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -35,6 +35,36 @@ fn main() {
     println!("The length of '{s2} is {len}.");
 
     // References and Borrowing
+    let s1 = String::from("hello");
+    let len = compute_length(&s1);
+    println!("The length of '{s1} is {len}");
+
+    // Mutable references
+    let mut s = String::from("hello");
+    change(&mut s);
+
+    {
+        let r1 = &mut s;
+    }
+    let r2 = &mut s;
+
+    let mut s = String::from("hello");
+    let r1 = &s;
+    let r2 = &s;
+    println!("{r1} and {r2}");
+
+    let r3 = &mut s;
+    println!("{r3}");
+
+    // Data References
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(",world");
+}
+
+fn compute_length(s: &String) -> usize {
+    s.len()
 }
 
 fn calculate_length(s: String) -> (String, usize) {
