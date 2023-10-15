28ddaea09fa67e03ce6febceb54aaec011f73525
Pedro Jorge <2230393@my.ipleiria.pt>
2023-10-15 09:34:52 +0100

    Enum enumeration examples and descriptions and method implementation

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -11,6 +11,17 @@ fn main() {
         kind: IpAddrKind::V6,
         address: String::from("::1"),
     };
+
+    let home = IpAddrV2::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrV2::V6(String::from("::1"));
+
+    let home = IpAddrV3::V4(127, 0, 0, 1);
+    let loopback = IpAddrV3::V6(String::from(":.1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    // The option Enum and Its Advantages over Null Values
 }
 
 enum IpAddrKind {
@@ -26,4 +37,22 @@ struct IpAddr {
 enum IpAddrV2 {
     V4(String),
     V6(String),
+}
+
+enum IpAddrV3 {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+
+    }
 }
