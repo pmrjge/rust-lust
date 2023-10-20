4606b318fbc428a83fa1b37b0a39d93a88ae7376
Pedro Jorge <pmrjge@gmail.com>
2023-10-20 23:01:03 +0100

    String assignment, concatenation and appending

--- common-collections/src/main.rs
+++ common-collections/src/main.rs
@@ -56,5 +56,49 @@ fn main() {
     } // from here v is released from memory
 
     // Storing UTF-8 encoded text with strings
+    // Creating a new string
+    let mut s = String::new();
+
+    let data = "initial contents";
+    let s = data.to_string();
+    let s = "initial contents".to_string();
+
+    let s = String::from("initial contents");
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שָׁלוֹם");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+
+    // Updating a string with push_str and push
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s2}");
+
+    let mut s = String::from("lo");
+    s.push('l');
+
+    // Concatenation with the + operator or the format! Macro
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world");
+    let s3 = s1 + &s2;
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
+
+    // Indexing into strings
 
 }
