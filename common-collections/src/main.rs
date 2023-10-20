ce8f8a04e5a8daffbe58545f299926c8e2b79054
Pedro Jorge <pmrjge@gmail.com>
2023-10-20 23:51:04 +0100

    Intro to hashmaps

--- common-collections/src/main.rs
+++ common-collections/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     // Creating a Vector
     let v: Vec<i32> = Vec::new();
@@ -49,7 +51,7 @@ fn main() {
     // Dropping a Vector Drops Its Elements
 
     {
-        let v = vec![1,2,3,4]
+        let v = vec![1,2,3,4];
 
         // manipulate v
 
@@ -110,4 +112,55 @@ fn main() {
     }
 
     // Hash Maps
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    // HashMaps and ownership
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    // Updating a Hash Map
+    // Overwrite a value
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{:?}", scores);
+
+    // Adding a key and value only if a key isn't present
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    // Updating a value based on the old value
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+
+    // Hashing functions => look into crates.io for better hashing or fitter algorithms
 }
