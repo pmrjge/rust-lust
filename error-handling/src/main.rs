34fc8e4e6362d3e967e99a93e7f4ced0b242c7b2
Pedro Jorge <pmrjge@gmail.com>
2023-10-21 10:47:01 +0100

    Alternatives to using match for Result<T,E>

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-
+use std::io::ErrorKind;
 
 fn main() {
     // panic!("crash and burn");
@@ -8,10 +8,40 @@ fn main() {
     // v[99];
     let greeting_file_result = File::open("files/hello1.txt");
 
-    let greeting_file = match greeting_file_result {
+    /*let greeting_file = match greeting_file_result {
         Ok(file) => println!("Hello World"),
         Err(error) => panic!("Problem opening the file: {:?}", error),
+    };*/
+
+    // Matching on Different Errors
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => {
+                match File::create("files/hello.txt") {
+                    Ok(fc) => fc,
+                    Err(e) => panic!("Problem creating the file {:?}", e),
+                }
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        }
     };
+    //Alternatives to using match with Result<T, E>
+
+    let greeting_file1 = File::open("files/hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    } );
+
+    // Shortcut for panic on error: unwrap and expect
+
 
 }
 
