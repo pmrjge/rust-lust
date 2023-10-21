3d5bdae4d934f3f92517c5ea267eb0c3bf8eeea9
Pedro Jorge <pmrjge@gmail.com>
2023-10-21 21:40:04 +0100

    First part of the small project

---
+++ first-small-project/src/main.rs
@@ -0,0 +1,36 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    // Reading a file
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    // Refactor to improve modularity and Error Handling
+
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() > 3 {
+            panic!("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config {query, file_path}
+    }
+}
+
+
