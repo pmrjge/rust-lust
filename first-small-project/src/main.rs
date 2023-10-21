cf76573d811113148c956d4730d025a4ee9467e3
Pedro Jorge <pmrjge@gmail.com>
2023-10-21 21:56:08 +0100

    About to split code to lib.rs

--- first-small-project/src/main.rs
+++ first-small-project/src/main.rs
@@ -1,4 +1,5 @@
 use std::{env, fs, process};
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,13 +12,17 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error {e}");
+        process::exit(1);
+    }
 
 }
 
-fn run(config:Config){
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+fn run(config:Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
     println!("With text:\n{contents}");
+    Ok(())
 }
 
 struct Config {
