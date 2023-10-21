003bb122a28ba4fa67b8ea04b3c61f06c6e07214
Pedro Jorge <pmrjge@gmail.com>
2023-10-21 13:12:51 +0100

    Implementation of Generics in function definitions

--- generic-types-traits-lifetimes/src/main.rs
+++ generic-types-traits-lifetimes/src/main.rs
@@ -1,3 +1,75 @@
+use std::cmp::PartialOrd;
+
 fn main() {
-    println!("Hello, world!");
+    // Removing Duplication By Extracting a Function
+    let number_list = vec![35, 50, 25, 100, 65];
+    let mut largest = &number_list[0];
+
+    for number in &number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largets number is {largest}");
+
+    let number_list = vec![102, 34, 6000, 89, 53, 2, 43, 8];
+
+    let mut largest = &number_list[0];
+
+    for number in &number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {largest}");
+
+    let number_list = vec![102, 32, 6000, 89, 54, 2, 43, 8];
+
+    println!("The largest number is {}", largest(&number_list));
+
+    // Generic Data Types
+    // In Function Definitions
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {result}");
 }
+
+fn find_largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if largest > item {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+
