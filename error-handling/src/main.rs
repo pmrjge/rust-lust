6c3868b7a9c7ae9bc1a2c1bb4c3f4530cec9e704
Pedro Jorge <pmrjge@gmail.com>
2023-10-21 11:45:53 +0100

    Finished ironing out the details about exceptions panic and error handling

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::{self,File};
 use std::io::{self, Read, ErrorKind};
+use std::net::IpAddr;
 
 fn main() {
     // panic!("crash and burn");
@@ -48,7 +49,27 @@ fn main() {
     // Where the ? operator can be used
 
     // To Panic or not to panic!
+    let home: IpAddr = "127.0.0.1".parse().expect("Hardcoded IP address should be valid");
+    // Guideline for error handling
+    // Creating custom types for validation
 
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+        Guess {value}
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
 }
 
 enum Result1<T,E> {
