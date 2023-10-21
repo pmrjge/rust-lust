bb2bc31488248c7345f14c0beb3a8e8c2d1ecf13
Pedro Jorge <pmrjge@gmail.com>
2023-10-21 21:47:01 +0100

    Implemented mostly the program using only logical functions

--- first-small-project/src/main.rs
+++ first-small-project/src/main.rs
@@ -1,18 +1,23 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    // Reading a file
+    run(config);
+
+}
+
+fn run(config:Config){
     let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    // Refactor to improve modularity and Error Handling
-
+    println!("With text:\n{contents}");
 }
 
 struct Config {
@@ -21,15 +26,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() > 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config {query, file_path}
+        Ok(Config {query, file_path})
     }
 }
 
