cb429357f4de963e646a870cbb171282d385b3e5
Pedro Jorge <pmrjge@gmail.com>
2023-10-21 16:54:26 +0100

    Completed generic types, traits and lifetimes

--- generic-types-traits-lifetimes/src/main.rs
+++ generic-types-traits-lifetimes/src/main.rs
@@ -1,4 +1,5 @@
 use std::cmp::PartialOrd;
+use std::fmt::Display;
 
 use generic_types_traits_lifetimes::news_article::{Summary, Tweet};
 
@@ -75,7 +76,43 @@ fn main() {
     // Using Trait Bounds to Conditionally Implement methods
 
     // Validating References With Lifetimes
+    // Lifetime annotation syntax
+    fn long_est<'a>(x: &'a str, y: &'a str) -> &'a str {
+        if x.len() > y.len() {x} else {y}
+    }
+    // thinking in terms of lifetimes
+    fn longest<'a>(x: &'a str, y: &str) -> &'a str {
+        x
+    }
 
+    // Lifetime annotations in struct definitions
+    struct ImportantExcerpt<'a> {
+        part: &'a str,
+    }
+    let novel = String::from(
+        "Call me Ishmael. Some years ago..."
+    );
+    let first_sentence = novel
+        .split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    // Lifetime Elision
+    // Lifetime annotations in method definitions
+    // The static lifetime
+    let s: &'static str = "I have a static lifetime.";
+    // Generic Types Parameters, Trait Bounds and LifeTimes Together
+    fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T,)-> &'a str
+    where T: Display
+    {
+        println!("Announcement! {ann}");
+        if x.len() > y.len() {
+            x
+        } else { y }
+    }
 }
 
 
